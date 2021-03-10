112ee2642233d8aeaf97109d4d69b7f892e57922
Vemund Dyrkolbotn <phizch@hotmail.com>
2021-03-10 03:43:15 +0100

    Add project files.

---
+++ UUDecode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UUDecode
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "Hello World!" );
+		}
+	}
+}
---
+++ UUDecode/UUEncoding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace UUDecode
+{
+	public class UUEncoding
+	{
+
+
+		public class Scalar
+		{
+
+		}
+
+		public class Vec128
+		{
+			public static bool IsSupported => Ssse3.IsSupported;
+
+		}
+
+		public class Vec256
+		{
+			public static bool IsSupported => Avx2.IsSupported;
+
+
+
+
+		}
+
+
+
+	}
+}
